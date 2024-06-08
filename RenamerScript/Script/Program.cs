ae7e237c17f44b7890bef1aec7744b28d10bd2bc
Kartik Dhar <61696585+kartikdhar44@users.noreply.github.com>
2024-06-09 00:43:50 +0530

    added a renamer script and restructured the project

---
+++ RenamerScript/Script/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string folderPath = "F:\\HomeTogether_v0.22\\HomeTogether_v0.22\\HomeTogether\\Character";
+       // string fileNameToRemove = "UserCreated";
+        int i=0;
+
+        string[] files = Directory.GetFiles(folderPath);
+        foreach (string filePath in files)
+        {
+            try{
+            FileInfo fileInfo = new FileInfo(filePath);
+            string fileName = fileInfo.Name;
+            string newFileName = i.ToString()+".png";
+            string destinationPath = Path.Combine(folderPath, newFileName);
+            if(File.Exists(destinationPath)){
+                 i++;
+                continue;
+            }
+            File.Move(filePath, destinationPath);
+            Console.WriteLine($"File '{filePath}' renamed to '{destinationPath}'.");
+            i++;
+            }
+            catch(Exception e){
+                Console.WriteLine(e.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
